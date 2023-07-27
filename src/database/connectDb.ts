3d5726e247251472217e38c0fc48b06b51e755bd
Matthew Degnan <38664414+discordwolf-bots@users.noreply.github.com>
2023-07-27 14:53:53 +0100

    Connected to db confirmation message

--- src/database/connectDb.ts
+++ src/database/connectDb.ts
@@ -1,10 +1,11 @@
-import { connect } from 'mongoose';
+import { connect, connection } from 'mongoose';
 import { ExtendedClient } from '../interfaces/ExtendedClient';
 import { errorHandler } from '../utils/errorHandler';
 
 export const connectDb = async (bot: ExtendedClient) => {
   try {
     await connect(bot.config.dbUri);
+    if (connection) console.log('Connected to Mongoose');
   } catch (err) {
     await errorHandler(bot, err, 'database connection');
   }
